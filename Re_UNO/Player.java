37f5434a8ee60469f7fff8976a28130b0d6936a7
MoogieMoo <yxia1@stuy.edu>
2017-05-20 19:54:41 -0400

    Add files via upload

---
+++ Re_UNO/Player.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Player
+{
+    private String name;
+    private ArrayList<Card> hand;
+        
+    /**
+     * Player constructor
+     */
+    public Player( String s ) {
+        name = s;
+        hand = new ArrayList<Card>();            
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+    
+    /**
+     * Add a card to the player's hand
+     */
+    public void addCard( Card card ) {
+        hand.add(card);
+    }
+    
+    /**
+     * Choose a card to play.
+     * for AI players
+     */
+    public Card playCard( Card placedCard ) {
+        
+        // play the first playable card we can find
+        for ( Card c : hand ) {
+            if (c.playable(placedCard)) {
+                hand.remove(c);
+                return c;
+            }
+        }        
+        // no playable card
+        return null;//skip turn
+        
+    }
+    
+}
